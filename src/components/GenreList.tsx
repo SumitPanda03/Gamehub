c549b01b7687e79e86efce1b4a6808eba49eed46
Sumit_Panda <95120820+SumitPanda03@users.noreply.github.com>
2023-12-30 09:21:13 +0530

    Genre Panel improved

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
     Button,
     HStack,
+    Heading,
     Image,
     List,
     ListItem,
@@ -22,6 +23,9 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
     return (
         <>
+            <Heading fontSize="2xl" marginBottom={3}>
+                Genres
+            </Heading>
             <List>
                 {data.map((genre) => (
                     <ListItem key={genre.id} paddingY={"5px"}>
@@ -29,9 +33,12 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
                             <Image
                                 boxSize={"32px"}
                                 borderRadius={8}
+                                objectFit={"cover"}
                                 src={getCroppedImageUrl(genre.image_background)}
                             />
                             <Button
+                                whiteSpace={"normal"}
+                                textAlign={"left"}
                                 fontWeight={
                                     genre.id === selectedGenre?.id
                                         ? "bold"
